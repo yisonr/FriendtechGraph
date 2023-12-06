6553d5b9d4a6787f1d56fb0729a969f1a03ce4d2
lujetliu <462546997@qq.com>
2023-12-06 10:34:11 +0800

    graph V0.0.2

--- src/friendtech-shares-v-1.ts
+++ src/friendtech-shares-v-1.ts
@@ -1,14 +1,15 @@
 import {
   OwnershipTransferred as OwnershipTransferredEvent,
-  Trade as TradeEvent
+  Trade as TradeEvent,
 } from "../generated/FriendtechSharesV1/FriendtechSharesV1"
-import { OwnershipTransferred, Trade } from "../generated/schema"
+import { OwnershipTransferred, Trade, Protocol, Subject, Holder } from "../generated/schema"
+import { BigDecimal, crypto, BigInt, Bytes, Address } from '@graphprotocol/graph-ts'
 
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent
 ): void {
   let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+    event.transaction.hash.toHexString()
   )
   entity.previousOwner = event.params.previousOwner
   entity.newOwner = event.params.newOwner
@@ -20,22 +21,69 @@ export function handleOwnershipTransferred(
   entity.save()
 }
 
+// export function createProtocol(protocol: Protocol): Protocol {
+//   return protocol as Protocol
+// }
+
+export function getOrCreateSubject(subjectId: string): Subject {
+  let subject = Subject.load(subjectId)
+  if (subject != null) {
+    return subject
+  }
+
+  subject = new Subject(subjectId)
+  subject.holdersCount = 0
+  subject.keySupply = BigInt.zero()
+  subject.save()
+  return subject
+}
+
+
+export function getOrCreateHolder(holderId: string, subject: Subject): Holder {
+  let holder = Holder.load(holderId)
+  if (holder != null) {
+    return holder
+  }
+
+  holder = new Holder(holderId)
+  holder.subject = subject.id
+  holder.count = BigInt.zero()
+  holder.save()
+  return holder
+}
+
 export function handleTrade(event: TradeEvent): void {
-  let entity = new Trade(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+  let trade = new Trade(
+    event.transaction.hash.toHexString()
   )
-  entity.trader = event.params.trader
-  entity.subject = event.params.subject
-  entity.isBuy = event.params.isBuy
-  entity.shareAmount = event.params.shareAmount
-  entity.ethAmount = event.params.ethAmount
-  entity.protocolEthAmount = event.params.protocolEthAmount
-  entity.subjectEthAmount = event.params.subjectEthAmount
-  entity.supply = event.params.supply
+  trade.trader = event.params.trader
+  trade.subject = event.params.subject
+  trade.isBuy = event.params.isBuy
+  trade.shareAmount = event.params.shareAmount
+  trade.ethAmount = event.params.ethAmount
+  trade.protocolEthAmount = event.params.protocolEthAmount
+  trade.subjectEthAmount = event.params.subjectEthAmount
+  trade.supply = event.params.supply
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  trade.blockNumber = event.block.number
+  trade.blockTimestamp = event.block.timestamp
+  trade.transactionHash = event.transaction.hash
+  trade.save()
 
-  entity.save()
+  // subject
+  let subjectId = event.params.subject.toHex()
+  let subject = getOrCreateSubject(subjectId)
+  subject.holdersCount = trade.isBuy? subject.holdersCount+1: subject.holdersCount-1
+  subject.keySupply = trade.supply
+  subject.save()
+  
+  // holder
+  let holderId = event.params.trader.toHex()
+  let holder = getOrCreateHolder(holderId, subject)
+  if (trade.isBuy) {
+	holder.holdingsTotal =  holder.holdingsTotal.plus(trade.shareAmount)
+  } else {
+	holder.holdingsTotal =  holder.holdingsTotal.minus(trade.shareAmount)
+  }
+  holder.save()
 }
