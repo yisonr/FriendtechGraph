2e1cb078f5377a06e68ccbb3d20d93c8531b0bbb
lujetliu <462546997@qq.com>
2023-12-12 14:53:00 +0800

    Many-To-Many Relationships

--- src/friendtech-shares-v-1.ts
+++ src/friendtech-shares-v-1.ts
@@ -2,7 +2,7 @@ import {
   OwnershipTransferred as OwnershipTransferredEvent,
   Trade as TradeEvent,
 } from "../generated/FriendtechSharesV1/FriendtechSharesV1"
-import { OwnershipTransferred, Trade, Protocol, Subject, Holder } from "../generated/schema"
+import { OwnershipTransferred, Trade, Protocol, Subject, Holder, SubjectHolder } from "../generated/schemav1"
 import { BigDecimal, crypto, BigInt, Bytes, Address } from '@graphprotocol/graph-ts'
 
 export function handleOwnershipTransferred(
@@ -21,9 +21,19 @@ export function handleOwnershipTransferred(
   entity.save()
 }
 
-// export function createProtocol(protocol: Protocol): Protocol {
-//   return protocol as Protocol
-// }
+function getOrCreateProtocol(protocolId: string): Protocol {
+    let protocol = Protocol.load(protocolId)
+    if (protocol == null) {
+        protocol = new Protocol(protocolId)
+        protocol.userCount = 0
+        protocol.protocolRevenue = BigDecimal.fromString('0')
+        protocol.accountRevenue = BigDecimal.fromString('0')
+        protocol.tradeVolume = BigDecimal.fromString('0')
+        protocol.totalTrades = 0
+        protocol.save()
+    }
+    return protocol as Protocol
+}
 
 export function getOrCreateSubject(subjectId: string): Subject {
   let subject = Subject.load(subjectId)
@@ -32,25 +42,41 @@ export function getOrCreateSubject(subjectId: string): Subject {
   }
 
   subject = new Subject(subjectId)
-  subject.holdersCount = 0
+  subject.holdersCount = BigInt.zero()
   subject.keySupply = BigInt.zero()
   subject.save()
   return subject
 }
 
 
-export function getOrCreateHolder(holderId: string, subject: Subject): Holder {
+export function getOrCreateHolder(holderId: string): Holder {
   let holder = Holder.load(holderId)
   if (holder != null) {
     return holder
   }
 
+
   holder = new Holder(holderId)
-  holder.subject = subject.id
-  holder.count = BigInt.zero()
+  holder.holdingsTotal = BigInt.zero()
   holder.save()
   return holder
 }
+
+export function getOrCreateSubjectHolder(subject: Subject, holder: Holder): SubjectHolder {
+  let id = subject.id.concat(holder.id)
+  let sh = SubjectHolder.load(id)
+  if (sh != null) {
+    return sh
+  }
+
+  sh = new SubjectHolder(id)
+  sh.subject = subject.id
+  sh.holder = holder.id
+  sh.count = BigInt.zero()
+  sh.save()
+  return sh
+}
+
 
 export function handleTrade(event: TradeEvent): void {
   let trade = new Trade(
@@ -70,20 +96,53 @@ export function handleTrade(event: TradeEvent): void {
   trade.transactionHash = event.transaction.hash
   trade.save()
 
+  let protocol = getOrCreateProtocol('1') // Assuming there is only one protocol
+  protocol.tradeVolume = protocol.tradeVolume.plus(BigDecimal.fromString(trade.ethAmount.toString()))
+  protocol.totalTrades = protocol.totalTrades + 1
+  protocol.protocolRevenue = protocol.protocolRevenue.plus(BigDecimal.fromString(trade.protocolEthAmount.toString()))
+  protocol.accountRevenue = protocol.accountRevenue.plus(BigDecimal.fromString(trade.subjectEthAmount.toString()))      
+  protocol.userCount = protocol.userCount + 1 
+  protocol.save()
+
   // subject
   let subjectId = event.params.subject.toHex()
   let subject = getOrCreateSubject(subjectId)
-  subject.holdersCount = trade.isBuy? subject.holdersCount+1: subject.holdersCount-1
   subject.keySupply = trade.supply
   subject.save()
   
   // holder
   let holderId = event.params.trader.toHex()
-  let holder = getOrCreateHolder(holderId, subject)
-  if (trade.isBuy) {
-	holder.holdingsTotal =  holder.holdingsTotal.plus(trade.shareAmount)
-  } else {
-	holder.holdingsTotal =  holder.holdingsTotal.minus(trade.shareAmount)
-  }
+  let holder = getOrCreateHolder(holderId)
   holder.save()
+
+  let subjectHolder = getOrCreateSubjectHolder(subject, holder)
+  if (subjectHolder.count == BigInt.zero() ) {
+    // 新建的 SubjectHolder
+	subjectHolder.count.plus(event.params.shareAmount)
+	subjectHolder.save()
+
+	// 添加 subject 的 holdersCount
+	subject.holdersCount.plus(BigInt.fromI32(1))
+	subject.save()
+	return
+  } 
+
+
+  if (event.params.isBuy) {
+	subjectHolder.count.plus(event.params.shareAmount)
+	subjectHolder.save()
+	return 
+  } 
+
+
+  if (subjectHolder.count == event.params.shareAmount) {
+	subject.holdersCount.minus(BigInt.fromI32(1))
+	subject.save()
+
+	subjectHolder.remove()
+	return
+  }
+  subjectHolder.count.minus(event.params.shareAmount)
+  subjectHolder.save()
+
 }
